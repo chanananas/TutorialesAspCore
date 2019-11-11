0919db1d67b9ce374ac255dba307019ac6746d74
Juan Pablo Almeida <palmeida@saludsa.com.ec>
2019-11-11 14:18:44 -0500

    Pone sonido

--- Hola Mundo/Program.cs
+++ Hola Mundo/Program.cs
@@ -7,7 +7,9 @@ namespace Hola_Mundo
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Blue;  
-            Console.WriteLine("Hello World!");            
+            Console.WriteLine("Hello World!");    
+            Console.Beep();
+            Console.ReadLine();
         }
     }
 }
